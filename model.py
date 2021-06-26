dd517753e06b1bf83dd7019ae1298d5b48b8fcfc
Sarthak Trivedi <sarthak.trivedi33@gmail.com>
2021-06-26 17:30:49 +0200

    Updated a bunch of things.

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Conv2D,Conv2DTranspose,Input,BatchNormalization,concatenate,MaxPooling2D,Activation
+from tensorflow.keras.backend import int_shape
+from tensorflow.keras.layers import Conv2D,Conv2DTranspose,Input,BatchNormalization,concatenate,MaxPooling2D,Cropping2D
 
 def conv2d_block(input,n_filters,kernel_size=3,stride=1,padding='valid',batch_norm=True,activation='relu'):
     """
@@ -17,10 +18,29 @@ def conv2d_block(input,n_filters,kernel_size=3,stride=1,padding='valid',batch_no
 
 def deconv2d_block(input,concatenation_tensor,n_filters,kernel_size=3,stride=1,padding='valid'):
     x = Conv2DTranspose(n_filters, kernel_size=kernel_size, strides=stride, padding=padding)(input)
+    ch, cw = get_crop_shape(int_shape(concatenation_tensor), int_shape(x))
+    conv = Cropping2D(cropping=(ch, cw))(concatenation_tensor)
     x = concatenate([x, concatenation_tensor])
     x = conv2d_block(input=x, n_filters=n_filters, batch_norm=False, padding=padding)
     return x
 
+def get_crop_shape(target, refer):
+    # width, the 3rd dimension
+    cw = target[2] - refer[2]
+    assert (cw >= 0)
+    if cw % 2 != 0:
+        cw1, cw2 = int(cw/2), int(cw/2) + 1
+    else:
+        cw1, cw2 = int(cw/2), int(cw/2)
+    # height, the 2nd dimension
+    ch = target[1] - refer[1]
+    assert (ch >= 0)
+    if ch % 2 != 0:
+        ch1, ch2 = int(ch/2), int(ch/2) + 1
+    else:
+        ch1, ch2 = int(ch/2), int(ch/2)
+
+    return (ch1, ch2), (cw1, cw2)
 def UNET(
     input_shape,
     num_classes=1,
@@ -32,16 +52,16 @@ def UNET(
     x = input
     down_layers = []
     for l in range(num_layers):
-        x = conv2d_block(input=x, n_filters=filters, padding='valid')
+        x = conv2d_block(input=x, n_filters=filters, padding='same')
         down_layers.append(x)
         x = MaxPooling2D((2, 2), strides=2)(x)
         filters = filters * 2
 
-    x = conv2d_block(input=x, n_filters=filters, batch_norm=False, padding='valid')
+    x = conv2d_block(input=x, n_filters=filters, batch_norm=False, padding='same')
 
     for conv in reversed(down_layers):
         filters //= 2  # decreasing number of filters with each layer
-        x = deconv2d_block(input=x, concatenation_tensor=conv,n_filters=filters, padding='valid')
+        x = deconv2d_block(input=x, concatenation_tensor=conv,n_filters=filters, padding='same')
     outputs = Conv2D(num_classes, (1, 1), activation=output_activation)(x)
 
     model = Model(inputs=[input], outputs=[outputs])
